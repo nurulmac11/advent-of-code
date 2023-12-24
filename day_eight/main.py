4547465e3d430769b7491037015dfbe54139ed32
Nurullah <nurullah.macun@ignitia.se>
2023-12-24 19:36:27 +0300

    solved day 8 part 2

--- day_eight/main.py
+++ day_eight/main.py
@@ -7,8 +7,11 @@ file_path = "input.txt"
 instrs = ""
 mapping = {}
 head_pivot = ""
+starters = []
+endings = set()
+
 def parse_file(file_path):
-    global instrs, mapping, head_pivot
+    global instrs, mapping, head_pivot, starters
     with open(file_path, 'r') as file:
         for i, line in enumerate(file):
             if i == 0:
@@ -21,23 +24,43 @@ def parse_file(file_path):
                 if i == 2:
                     head_pivot = key
                 mapping[key] = values
-    
-def process(pivot):
+                if key[-1] == 'A':
+                    starters.append(key)
+                if key[-1] == 'Z':
+                    endings.add(key)
+def ending_check(pivots):
+    global endings
+    for pivot in pivots:
+        if pivot not in endings:
+            return False
+    return True
+
+def process(pivots):
     global mapping, instrs
     step = 0
     current_i = 0
-    while pivot != 'ZZZ':
+    while not ending_check(pivots):
         current_i %= len(instrs)
-        current = mapping[pivot]
+        new_pivots = []
+        currents = [mapping[pivot] for pivot in pivots]
         c = instrs[current_i]
         if c == 'R':
-            pivot = current[1]
+            new_pivots = [current[1] for current in currents]
         elif c == 'L':
-            pivot = current[0]
+            new_pivots = [current[0] for current in currents]
+        pivots = new_pivots
         step += 1
         current_i += 1
+        print(step, pivots)
     return step
 
 parse_file(file_path)
-r = process(head_pivot)
-print(r)
+print(starters)
+print(endings)
+per = []
+import math
+for s in starters:
+    r = process([s])
+    per.append(r)
+print(per)
+print(math.lcm(*per))
